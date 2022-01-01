fd9a72649118592c7b36643c2fb5c23615aa849b
Samir Mahapatra <samirmahapatra16@gmail.com>
2022-01-01 18:16:06 +0530

    New File Created In Assignment-07

---
+++ Assignment-07/src/class_note_4/note.java
@@ -0,0 +1,11 @@
+package class_note_4;
+
+import java.util.Scanner;
+
+public class note {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.next();
+        System.out.println(n);
+    }
+}
